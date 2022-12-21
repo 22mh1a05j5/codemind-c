b0f6b9dea2edf7d5fd4b20f00a3a3833cc8b61d1
sriram.munaga <119722906+22mh1a05j5@users.noreply.github.com>
2022-12-21 21:21:57 +0530

    #include<stdio.h> int main() {     int i,n,m,c=0;     scanf("%d%d",&n,&m);     for(i=n;i<=m;i++)     {         if(i%3==0)         {        c=c+1;         }     }     printf("%d",c); }

---
+++ count_Multiples_of_3_between_M_,N.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,m,c=0;
+    scanf("%d%d",&n,&m);
+    for(i=n;i<=m;i++)
+    {
+        if(i%3==0)
+        {
+       c=c+1;
+        }
+    }
+    printf("%d",c);
+}
