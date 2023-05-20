15be0a494cf4892572fa7b5773d323304b39a3bf
sriram.munaga <119722906+22mh1a05j5@users.noreply.github.com>
2023-05-20 20:30:48 +0530

    #include<stdio.h> int main(){     int n;     scanf("%d",&n);     while(n!=0){         n--;         int k,i,f=1;         scanf("%d",&k);         for(i=1;i<=k;i++){             f*=i;         }         printf("%d ",f);     } }

---
+++ Small_Factorials.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    while(n!=0){
+        n--;
+        int k,i,f=1;
+        scanf("%d",&k);
+        for(i=1;i<=k;i++){
+            f*=i;
+        }
+        printf("%d
+",f);
+    }
+}
