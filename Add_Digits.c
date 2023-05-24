7b623e6c12595394485afe8a3e33a6dd4b53a814
sriram.munaga <119722906+22mh1a05j5@users.noreply.github.com>
2023-05-24 21:51:35 +0530

    #include<stdio.h> int main() {     int n,r,s=0;     scanf("%d",&n);     while(n!=0)     {         r=n%10;         s=s+r;         n=n/10;         if(n==0&&s>9)         {             n=s;             s=0;         }     }     printf("%d",s); }

--- Add_Digits.c
+++ Add_Digits.c
@@ -1,13 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int n,r;
+    int n,r,s=0;
     scanf("%d",&n);
-    while(n>9)
+    while(n!=0)
     {
         r=n%10;
+        s=s+r;
         n=n/10;
-        n=n+r;
+        if(n==0&&s>9)
+        {
+            n=s;
+            s=0;
+        }
     }
- printf("%d",n);   
+    printf("%d",s);
 }
